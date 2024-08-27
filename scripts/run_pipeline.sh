67b8add012b2097dbcf5cd4994c481337e243e91
Lezek123 <leszek@jsgenesis.com>
2024-08-27 16:52:29 +0200

    Make run_pipeline more adjustable

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -8,18 +8,9 @@ set -x
 
 # TODO: Setup a remote for DVC
 
-STAGES="$1"
-C_FLAG="$2"
-
-if [ $C_FLAG == "-c" ]; then
-    FLAGS="-c job"
-else
-    FLAGS=""
-fi
-
 python3 -m musicnet.main $FLAGS\
         --config-path="$SCRIPT_PATH" \
         --config-name=defaults \
-        stages=[$STAGES] \
+        $@
         # Can add some overrides here, like:
         # model.n_neurons=200 \
