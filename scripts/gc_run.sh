3f93e73559196c35bb0e6e04981f74abb8257a29
Lezek123 <leszek@jsgenesis.com>
2024-09-02 22:29:13 +0200

    DVC default remote on Cloud

--- scripts/gc_run.sh
+++ scripts/gc_run.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH="$(dirname $0)"
 cd $SCRIPT_PATH/..
 
-dvc remote add origin /gcs/musicnet-dvc
+dvc remote add origin -d /gcs/musicnet-dvc
 ./scripts/run_pipeline.sh stages=[GENERATE,PREPROCESS,TRAIN,EVAL] exp=True
 dvc exp push origin
