9ce214197acd93a2b7cce880c9ccb92efb954175
Lezek123 <leszek@jsgenesis.com>
2024-09-12 18:54:40 +0200

    Fix gc_run script

--- scripts/gc_run.sh
+++ scripts/gc_run.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$(dirname $0)"
 cd $SCRIPT_PATH/..
 
 dvc remote add origin -d /gcs/musicnet-dvc
-./scripts/run_pipeline.sh stages=[GENERATE,PREPROCESS,TRAIN,EVAL] exp=True
+./scripts/run_pipeline.sh exp=True
 dvc exp push origin
